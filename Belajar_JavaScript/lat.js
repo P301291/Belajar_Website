1fbaba125699ea36013c816af734ac12266ef09e
softwarestorecandra1234-cloud <software.store.candra1234@gmail.com>
2026-02-06 19:14:37 +0700

    first commit

---
+++ Belajar_JavaScript/lat.js
@@ -0,0 +1,14 @@
+ let x = 10;
+            let y = 3;
+
+            let hasilTambah = x + y;
+            let hasilBagi = x / y;
+            const hasilKali = x * y;
+            var hasilKurang = x - y;
+           
+            alert("Mari Belajar")
+            document.write("hasil:", hasilBagi);
+            document.getElementById("output").innerHTML = "Data ditampilkan" + hasilKali;
+            document.getElementById("input").innerHTML = "Hasil," + hasilBagi;
+            document.write("hasil:", hasilKurang);
+            console.log("Hasil:", hasilKurang);
---
+++ Belajar_php/lat1.php
@@ -0,0 +1,16 @@
+<?php
+$a = 10;
+$b = 3;
+
+$tambah = $a + $b;
+$kurang = $a + $b;
+print "aku mau belajar";
+print "<br>";
+print "<br>";
+ echo "Hasil Tambah:"," " . $tambah;
+ echo"<br>";
+ echo"<br>";
+ echo "Hasil"," " . $kurang;
+ echo "<br>";
+
+ ?>
