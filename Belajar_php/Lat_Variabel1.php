de1e876d568b38484f946377190aab844e11e4b7
softwarestorecandra1234-cloud <software.store.candra1234@gmail.com>
2026-02-11 21:51:14 +0700

    ada perubahan

---
+++ Belajar_php/Lat_Variabel1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Belajar variabel1</title>
+</head>
+<body>
+<h1>Belajar_Variabel Ke 1<h1>
+
+    <?php
+    $x = 6; //integer
+    $y = "@Jhon1234";//string
+    $o = "IRWAN";
+    $i = "Nasi Goreng";
+    $Objek1 = 1;
+    $Objek2 = 2;
+    $Hasil = $Objek1 + $Objek2;
+  
+
+    echo $x;
+    echo "<br>";
+    echo $y;
+    echo "<br>";
+    echo $o;
+    echo "<br>";
+    echo "Saya Mau Makan $i";
+    echo "<br>";
+    echo 'Saya Mau Makan'.' '. $i.'';
+    echo "<br>";
+    echo $Hasil;
+    echo "<br>";
+    echo $Objek1 + $Objek2;
+    ?>
+
+</body>
+</html>
