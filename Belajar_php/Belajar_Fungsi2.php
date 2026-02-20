88751e7ecbcb00627e38fb1c7d29a5aa9fe324cb
softwarestorecandra1234-cloud <software.store.candra1234@gmail.com>
2026-02-20 18:50:55 +0700

    perubahan1

---
+++ Belajar_php/Belajar_Fungsi2.php
@@ -0,0 +1,12 @@
+<?php
+function nama_fungsi($nama){
+    return "$nama argadinata.<br>";
+
+  
+}
+
+echo nama_fungsi("Rama");
+echo nama_fungsi("Dawi");
+echo nama_fungsi("Irwan");
+
+?>
---
+++ Belajar_php/Belajar_Pungsi1.php
@@ -0,0 +1,8 @@
+<?php
+function myMessage(){
+    echo " Hello World";
+    echo"<br>";
+    echo"Aku Datang";
+}
+myMessage();
+?>
